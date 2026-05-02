12460830f9efc3364a4bd0f349df856535f70255
Helal Uddin -SWE <seoguro1999@gmail.com>
2026-05-02 15:11:21 +0600

    s

---
+++ 01 Foundations & Security/step-01-environment-setup/server.js
@@ -0,0 +1,9 @@
+const express=require('express')
+const app=express()
+const PORT=5000
+app.get('',(req,res)=>{
+  res.send(" API is running")
+})
+app.listen(PORT,()=>{
+  console.log(`server running on http://localhost:${PORT}`)
+})
