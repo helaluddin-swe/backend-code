3932dc314bc3cce2ca82c6f8c3fc40dc590f1f1c
Helal Uddin -SWE <seoguro1999@gmail.com>
2026-05-02 15:34:01 +0600

    add basic setup

--- 01 Foundations -Security/step-01-environment-setup/server.js
+++ 01 Foundations -Security/step-01-environment-setup/server.js
@@ -1,9 +1,11 @@
 const express=require('express')
 const app=express()
-const PORT=5000
+const PORT=5001
 app.get('',(req,res)=>{
   res.send(" API is running")
 })
+// middleware for parse json data 
+app.use(express.json())
 app.listen(PORT,()=>{
   console.log(`server running on http://localhost:${PORT}`)
 })
